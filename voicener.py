7d08935afc38a5d90005aac32249f310d16751c8
Alex Lan <90366760+2021alexl@users.noreply.github.com>
2023-03-23 23:48:41 -0400

    Update voicener.py

--- voicener.py
+++ voicener.py
@@ -31,19 +31,7 @@ def main():
             pause_threshold=3.0
         )
     if audio_bytes:
-        s = BytesIO(audio_bytes)
-        AudioSegment.from_file(BytesIO(audio_bytes)).export('audio.mp3', format='mp3')
-        filename = "audio.mp3"
-        w_audio = whisper.load_audio(filename)
-        pad_w_audio =whisper.pad_or_trim(w_audio)
-        torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        model = whisper.load_model("small.en")
-        model = model.to(torch_device)
-        mel = whisper.log_mel_spectrogram(pad_w_audio).to(model.device)
-        decode_options = dict(language="en")
-        transcribe_options = dict(task="transcribe", **decode_options)
-        transcription = model.transcribe(filename, **transcribe_options)
-        result = transcription["text"]
+        result = audio_bytes
         st.write(result)
         if result: 
             m = st.text_input('-', 'none', 50)

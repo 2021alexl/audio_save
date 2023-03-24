86e33957bf70ebb068420903746453fc9c9d13bf
Alex Lan <90366760+2021alexl@users.noreply.github.com>
2023-03-23 23:57:33 -0400

    Add files via upload

--- voicener.py
+++ voicener.py
@@ -5,16 +5,16 @@ import whisper
 from audio_recorder_streamlit import audio_recorder
 import io
 from pydub import AudioSegment
+from pydantic import BaseModel
+from pydub import AudioSegment
 from io import BytesIO
 import whisper
+import base64
+import torch
 import numpy as np
 import pandas as pd 
 import os
-import torch
 import ffmpeg
-import speech_recognition as sr
-
-
 def download_link(content, filename):
     """Function to create a download link for a given string."""
     # Create a hyperlink with the given content and filename
@@ -31,17 +31,29 @@ def main():
             pause_threshold=3.0
         )
     if audio_bytes:
-        result = audio_bytes
+        s = BytesIO(audio_bytes)
+        AudioSegment.from_file(BytesIO(audio_bytes)).export('audio.mp3', format='mp3')
+        filename = "audio.mp3"
+        w_audio = whisper.load_audio(filename)
+        pad_w_audio =whisper.pad_or_trim(w_audio)
+        torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        model = whisper.load_model("small.en")
+        model = model.to(torch_device)
+        mel = whisper.log_mel_spectrogram(pad_w_audio).to(model.device)
+        decode_options = dict(language="en")
+        transcribe_options = dict(task="transcribe", **decode_options)
+        transcription = model.transcribe(filename, **transcribe_options)
+        result = transcription["text"]
         st.write(result)
         if result: 
-            m = st.text_input('-', 'none', 50)
+            m = st.text_input('-', 'this part is the label', 50)
             if m != 'none':
                 result += ' | '
                 result += m
                 st.write(result)
-                filename = m
-                filename += '.txt'
-                st.download_button('Download some text', result)
+                file_name = st.text_input('1', 'this part is the file_name -- suggest using 1,2,3,4,5', 40)
+                if file_name:
+                    st.download_button('Download some text', result,file_name=file_name)
         
             
 

7b90e292f413d452bb9541c19aba7661c9383ade
Alex Lan <90366760+2021alexl@users.noreply.github.com>
2023-03-23 23:04:19 -0400

    Update voicener.py

--- voicener.py
+++ voicener.py
@@ -34,12 +34,16 @@ def main():
         s = BytesIO(audio_bytes)
         AudioSegment.from_file(BytesIO(audio_bytes)).export('audio.mp3', format='mp3')
         filename = "audio.mp3"
-        with sr.AudioFile('audio.mp3') as source:
-            audio_data = r.record(source)
-        
-        r = sr.Recognizer()
-        text = r.recognize_google(audio_data, language='en-US')
-        
+        w_audio = whisper.load_audio(filename)
+        pad_w_audio =whisper.pad_or_trim(w_audio)
+        torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        model = whisper.load_model("small.en")
+        model = model.to(torch_device)
+        mel = whisper.log_mel_spectrogram(pad_w_audio).to(model.device)
+        decode_options = dict(language="en")
+        transcribe_options = dict(task="transcribe", **decode_options)
+        transcription = model.transcribe(filename, **transcribe_options)
+        result = transcription["text"]
         st.write(result)
         if result: 
             m = st.text_input('-', 'none', 50)
